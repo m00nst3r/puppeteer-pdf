cffe49d6e0d9fb8d718c316ee9c13d9835393d1c
Andrii Kolisnyk <scear.sic@gmail.com>
2017-11-07 16:40:21 +0100

    Add headers to all calls

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ const PORT = process.env.PORT || 3333;
 
 const app = express();
 
+app.use((req, res, next) =>{
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 storage.initSync();
