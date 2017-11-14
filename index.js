92f7dc1d5657a8c42421c75a4f1cebfe86681d3a
Andrii Kolisnyk <scear.sic@gmail.com>
2017-11-14 17:16:24 +0100

    change some settings and improve logging

--- index.js
+++ index.js
@@ -22,23 +22,28 @@ function generatePdf(url, req, res) {
     const {settings} = req.body;
     const config = typeof settings !== "undefined" ? settings : null;
     async function get (auth, config) {
+        console.log(`request started at ${new Date()}`);
         const options = {
-            waitUntil: 'networkidle',
-            format:  config.format || 'A4',
             landscape: typeof config.landscape  !== "undefined" ? config.landscape : false,
             printBackground: typeof config.printBackground  !== "undefined" ? config.printBackground : true,
             displayHeaderFooter: typeof config.displayHeaderFooter !== "undefined" ? config.displayHeaderFooter : true,
-            timeout: 0,
-            headless: true
+            networkSetting: {
+                timeout: 0,
+                waitUntil: 'networkidle'
+            }
         };
         const browser = await puppeteer.launch({
-            headless: options.headless,
-            timeout: options.timeout,
+            headless: true,
+            timeout: options.networkSetting.timeout,
             args: ['--no-sandbox', '--disable-setuid-sandbox']
         });
         const page = await browser.newPage();
-        await page.setViewport({width: 1280, height: 0, deviceScaleFactor: 1});        
-        await page.goto(url, {waitUntil: options.waitUntil, timeout: options.timeout});
+        await page.setViewport({
+            width: 1280,
+            height: config.pageSize.height || 0,
+            deviceScaleFactor: 1
+        });
+        await page.goto(url, options.networkSetting);
         await page.evaluate(jwt => {
             localStorage.setItem('full-auth', jwt);
             localStorage.setItem('userName', jwt);
@@ -51,11 +56,11 @@ function generatePdf(url, req, res) {
             };
         });
         console.log(dimensions);
-        await page.reload({waitUntil: options.waitUntil, timeout: options.timeout});
-        await page.waitForNavigation({waitUntil: options.waitUntil, timeout: options.timeout});
+        await page.reload();
+        await page.waitForNavigation(options.networkSetting);
         await page.pdf({
             path: 'public/example.pdf',
-            // format: options.format,
+            format: config.format || 'A4',
             scale: 1,
             landscape: options.landscape,
             printBackground: options.printBackground,
@@ -63,19 +68,22 @@ function generatePdf(url, req, res) {
             height: dimensions.height,
             width: dimensions.width
         });
+        // await page.close();
         await browser.close();
     }
     get(Auth, config).then(v => {
         console.log('pdf generated');
+        console.log(`request finished at ${new Date()}`);
         res.json({"response": "ok"})
     }).catch(err => {
+        console.log(`request failed at ${new Date()}`);
         console.log(err);
-        res.json({"response": "error"});
+        res.json({"response": err});
     })
 }
 
 app.post('/', (req, res) => {
-    console.log(req.body);
+    console.log(`requested url: ${req.body.url}`);
     if (req.body.url) {
         generatePdf(req.body.url, req, res);
     } else {
